0accfa191a633ccd91b488fa049829686c4e5692
liavjulio <117514706+liavjulio@users.noreply.github.com>
2024-07-16 16:24:15 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -5,6 +5,6 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI =  os.getenv('DATABASE_URL') 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')  # Ensure OPENAI_API_KEY is set in your environment variables
+    OPENAI_API_KEY = os.getenv('OPENAI_API_KEY') 
     
 
